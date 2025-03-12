68d5b3b5cbb2c3369c335a9f8c87dd011219ee44
Chen Xiaohui <ysucxh@163.com>
2025-03-12 13:28:22 +0800

    add fmt pkg

--- magazine.go
+++ magazine.go
@@ -1,26 +1,28 @@
 // Package magazine
 package magazine
 
+import "fmt"
+
 type Subscriber struct {
 	Name   string
 	Rate   float64
 	Active bool
 }
 
-func PrintInfo(s *subscriber) {
+func PrintInfo(s *Subscriber) {
 	fmt.Println("Name:", s.name)
 	fmt.Println("Monthly rate", s.rate)
 	fmt.Println("Active?", s.active)
 }
 
-func DefaultSubscriber(name string) *subscriber {
-	var s subscriber
+func DefaultSubscriber(name string) *Subscriber {
+	var s Subscriber
 	s.name = name
 	s.rate = 5.99
 	s.active = true
 	return &s
 }
 
-func ApplyDiscount(s *subscriber) {
+func ApplyDiscount(s *Subscriber) {
 	s.rate = 4.99 // 使用点运算符在struct指针和struct上都可访问字段
 }
