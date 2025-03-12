ef743c90e8607f29f254345f72eed01bfb28289d
Chen Xiaohui <ysucxh@163.com>
2025-03-12 14:37:18 +0800

    nested struct

--- magazine.go
+++ magazine.go
@@ -3,10 +3,24 @@ package magazine
 
 import "fmt"
 
+type Address struct {
+	Street     string
+	City       string
+	State      string
+	PostalCode string
+}
+
 type Subscriber struct {
-	Name   string
-	Rate   float64
-	Active bool
+	Name        string
+	Rate        float64
+	Active      bool
+	Address
+}
+
+type Employee struct {
+	Name        string
+	Salary      float64
+	Address
 }
 
 func PrintInfo(s *Subscriber) {
